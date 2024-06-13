c7ecccac2761a6ef8881d0fcfabf4cb7273c06e3
ilexey <alexeyshitenkov@yandex.ru>
2024-06-13 17:16:04 +0300

    Updates

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import { registerUser } from '../redux/features/auth/authSlice'
+import { toast } from 'react-toastify'
 
 export const RegisterPage = () => {
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const {status} = useSelector((state => state.auth))
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    if (status) {
+      toast(status)
+    }
+  }, [status])
+
+  const handleSubmit = () => {
+    try {
+      dispatch(registerUser({username, password}))
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <form
-      onSubmit={e => e.preventDefault}
+      onSubmit={e => e.preventDefault()}
       className='w-1/4 h-60 mx-auto mt-40'
     >
       <h1 className='text-lg text-white text-center'>
@@ -15,6 +39,8 @@ export const RegisterPage = () => {
         <input
           className='mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700'
           type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
           placeholder='Username'
         />
       </label>
@@ -23,6 +49,8 @@ export const RegisterPage = () => {
         <input
           className='mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700'
           type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
           placeholder='Password'
         />
       </label>
@@ -30,6 +58,7 @@ export const RegisterPage = () => {
         <button
           className='flex justify-center items-center text-xs bg-gray-600 text-white rounded-sm py-2 px-4'
           type='submit'
+          onClick={handleSubmit}
         >
           Подтвердить
         </button>
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from '../../../utils/axios'
 
 const initialState = {
   user: null,
@@ -7,10 +8,44 @@ const initialState = {
   status: null
 }
 
+export const registerUser = createAsyncThunk('auth/registerUser',
+  async ({username, password}) => {
+    try {
+      const {data} = await axios.post('/auth/register', {
+        username,
+        password
+      })
+      if (data.token) {
+        window.localStorage.setItem('token', data.token)
+      }
+      return data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(registerUser.pending, (state) => {
+      state.isLoading = true;
+      state.status = null;
+    })
+    .addCase(registerUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.status = action.payload.message;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    })
+    .addCase(registerUser.rejected, (state, action) => {
+      state.status = action.payload.message;
+      state.isLoading = false;
+    })
+  }
 })
 
 export default authSlice.reducer
